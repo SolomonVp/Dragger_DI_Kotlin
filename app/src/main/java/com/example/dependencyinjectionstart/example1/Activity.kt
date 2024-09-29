7c18695547fe8a6a4e387b4083943f21e334d7cb
SolomonVP <Lum7023@yandex.ru>
2024-09-29 21:33:58 +0300

    Module и Provides

--- app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
@@ -5,11 +5,7 @@ import javax.inject.Inject
 class Activity {
 
     @Inject
-    lateinit var keyboard : Keyboard
-    @Inject
-    lateinit var monitor : Mouse
-    @Inject
-    lateinit var mouse : Monitor
+    lateinit var computer: Computer
 
     init {
         DaggerNewComponent.create().inject(this)
--- app/src/main/java/com/example/dependencyinjectionstart/example1/NewComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/NewComponent.kt
@@ -2,14 +2,14 @@ package com.example.dependencyinjectionstart.example1
 
 import dagger.Component
 
-@Component
+@Component(modules = [ComputerModule::class])
 interface NewComponent {
 
-    fun getKeyBoard(): Keyboard
-
-    fun getMouse(): Mouse
-
-    fun getMonitor(): Monitor
+//    fun getKeyBoard(): Keyboard
+//
+//    fun getMouse(): Mouse
+//
+//    fun getMonitor(): Monitor
 
     fun inject(activity: Activity)
 
